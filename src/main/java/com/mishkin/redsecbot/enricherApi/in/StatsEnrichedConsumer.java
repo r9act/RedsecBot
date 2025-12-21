54fe7ef694f8b5bb61a4d38d73c43e7c41839c72
a.mishkin <a.mishkin@argustelecom.ru>
2025-12-21 13:58:36 +0300

    Task-4 -добавлен Redis для дедупликации (idempotency) с TTL=6ч

--- src/main/java/com/mishkin/redsecbot/enricherApi/in/StatsEnrichedConsumer.java
+++ src/main/java/com/mishkin/redsecbot/enricherApi/in/StatsEnrichedConsumer.java
@@ -1,5 +1,6 @@
 package com.mishkin.redsecbot.enricherApi.in;
 
+import com.mishkin.redsecbot.enricherApi.idempotancy.DedupStore;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -12,13 +13,19 @@ import org.springframework.stereotype.Component;
 public class StatsEnrichedConsumer {
     private static final Logger log = LoggerFactory.getLogger(StatsEnrichedConsumer.class);
     private final DiscordReplyRegistry registry;
+    private final DedupStore dedupStore;
 
-    public StatsEnrichedConsumer(DiscordReplyRegistry registry) {
+    public StatsEnrichedConsumer(DiscordReplyRegistry registry, DedupStore dedupStore) {
         this.registry = registry;
+        this.dedupStore = dedupStore;
     }
 
     @KafkaListener(topics = "stats.enriched", groupId = "redsec-bot-enriched")
     public void consume(StatsEnrichedEvent event) {
+        if (!dedupStore.claim(event.correlationId())) {
+            return;
+        }
+
         var hookOpt = registry.take(event.correlationId());
 
         hookOpt.ifPresent(hook ->
